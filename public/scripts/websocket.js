94cb0bdb1ae6961304bae3c222bae954b880c4b2
attributeofextension <leah.carr00@gmail.com>
2018-01-05 19:57:03 +1000

    good to go aside heroku complications with ws port

---
+++ public/scripts/websocket.js
@@ -0,0 +1,165 @@
+var socket;  //Variable for socket; initialized in DOMContentLoaded event (to make connection)
+
+//Processes data from stock and returns an array compatible with High Charts Series format
+function processStockData(stock) {
+  stockData = [];
+  stockKeys = [];
+
+  stockKeys = Object.keys(stock);
+  for(var i = 0; i < stockKeys.length; i++) {
+    var day = [];
+    var date = new Date(stockKeys[i]);
+ //   var prices = stock[stockKeys[i]];
+    if(stock[stockKeys[i]]) {
+      day.push(date.getTime());
+      day.push(Number(stock[stockKeys[i]]["4. close"]));
+      stockData.push(day);
+    }
+  }
+
+  return stockData.sort( function(a,b) {
+    return a[0] - b[0];
+  });
+}
+//Makes the "HighStock" chart according to stocks (stockList)
+function makeChart(stocks) {
+  var chartData = [];
+  
+  for(var i = 0; i < stocks.length; i++) {
+    chartData.push( {
+      name: stocks[i].index,
+      data: processStockData(stocks[i].data),
+      tooltip: {
+        valueDecimals: 2
+      }
+    });
+  }
+  Highcharts.stockChart('container', {
+  
+    rangeSelector: {
+        selected: 1
+      },
+
+     title: {
+        text: 'Daily Close Price'
+     },
+    
+    series: chartData
+  });
+}
+//Clears stockList (for before a new update)
+function clearStockList() {
+  var ul = document.getElementById("stockList");
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+
+}
+//Updates the interface according to stockList
+//Should be a panel for each stock with a close button
+function updateStockList(stockList) {
+  var row = document.getElementById("stockList");
+
+  for(var i = 0; i < stockList.length; i++ ) {
+    var col = document.createElement("div");
+    col.setAttribute("class","col-md-2");
+    row.appendChild(col);
+
+    var panel = document.createElement("div");
+    panel.setAttribute("class","panel panel-default");
+    col.appendChild(panel);
+
+    var panelBody = document.createElement("div");
+    panelBody.setAttribute("class","panel-body");
+    panel.appendChild(panelBody);
+    panelBody.textContent = stockList[i].index;
+    
+    var btn = document.createElement("button");
+    btn.setAttribute("type","button");
+    btn.setAttribute("class","close");
+    btn.setAttribute("aria-label","Close");
+    btn.setAttribute("value",stockList[i].index);
+    panelBody.appendChild(btn);
+
+    var span = document.createElement("span");
+    span.setAttribute("aria-hidden","true");
+    span.innerHTML = "&times;";
+
+    btn.appendChild(span);
+    
+    btn.addEventListener("click",closeBtnHandler);
+    console.log(btn.value);
+  }
+
+
+}
+//Routes all messages received from server
+function messageRouter(data) {
+  switch(data.message) {
+    case 10: 
+      clearStockList();  
+      updateStockList(data.stockList);
+      makeChart(data.stockList);
+      break;
+  
+    case 40:
+      inputAlert(data.caption);
+      break;
+    case 41:
+      inputAlert(data.caption);
+      break;
+  }
+}
+//EVENT HANDLERS================================================================================
+//Handles some incoming messaage events that pertain to errors
+function inputAlert(caption) {
+  console.log(caption);
+  var alert = document.getElementById("alert");
+  alert.classList.remove("invisible");
+  var alertCaption = document.getElementById("alertCaption");
+  alertCaption.textContent = caption;
+}
+//Handles addBtn click event
+function addBtnHandler(event) {
+  document.getElementById("alert").classList.add("invisible");
+  
+  var stockInput = document.getElementById("stockInput");
+  if(stockInput.checkValidity()) {
+    var message = {"message":20,"stock":stockInput.value.toUpperCase()};
+    socket.send(JSON.stringify(message));
+  } else {
+    document.getElementById("alert").textContent = "Input must be all Upper Case and 5 characters maximum";
+    document.getElementById("alert").classList.remove("invisible");
+  }
+}
+//Handles closeBtn click event
+function closeBtnHandler(event) {
+  document.getElementById("alert").classList.add("invisible");
+  var message = {"message":21,"stock":event.target.parentElement.value};
+
+  socket.send(JSON.stringify(message));
+}
+//MAIN FUNCTION=================================================================================
+function loaded(event) {
+  socket = new WebSocket('ws://localhost:8081')
+
+  socket.addEventListener('open', function (event) {
+
+  });
+
+  socket.addEventListener('message', function (event) {
+   // console.log('Message from server ', event.data);
+    messageRouter(JSON.parse(event.data));
+  });
+  
+  window.onbeforeunload = function() {
+    socket.close();
+  };
+
+  var addBtn = document.getElementById("addBtn");
+  addBtn.addEventListener("click",addBtnHandler);
+
+}
+
+
+$("document").ready(loaded);
--- server.js
+++ server.js
@@ -1,10 +1,147 @@
 //FCC Stock Market Challenge https://www.freecodecamp.org/challenges/chart-the-stock-market
 const express = require("express");
 var app = express();
+//Conifgure Request
+const request = require('request');
 
+function makeRequestURL(symbol) {
+  return "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + symbol + "&apikey=Q56R8K057PXH6KB3";
+}
+
+//ROUTES
 app.use(express.static('public'));
-
 //PORT==========================================================================
-var listener = app.listen(80, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+//WEB SOCKET
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8081 });
+
+//StockList Data
+var stockList = [];
+
+wss.on('connection', function connection(ws) {
+  pushStockList();
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    messageRouter(JSON.parse(message));
+  });
+
+  //SOCKET MESSAGE ROUTING
+  function messageRouter(data) {
+    switch(data.message) {
+        case 20 :
+          checkStock(data.stock,success20,failure20);
+          function success20() {
+            pushStockList();
+            pushStockListAll();
+          }
+          function failure20() {
+            invalidStockAlert();
+          }
+         break;
+        case 21 : 
+          removeStockList(data.stock);
+          pushStockList();
+          pushStockListAll();
+        break;
+    }
+  }
+  //Checks to see if stock exists then makes a request to Alphavantage API
+  //If Alphavantage returns a positive result, callback "SUCCESS" is executed
+  //If Alphavantage returns a negative result, callback "FAILURE" is executed
+  function checkStock(stock,success,failure) {
+    //Check to see if stock already included,
+    //if it is then send stockAlreadyAlert message and terminate
+    if(stockExists(stock)) {
+      stockAlreadyAlert();
+      return;
+    }
+    //Make request
+    request(makeRequestURL(stock), function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+      if(error) {
+        console.log("Error retrieving Stock Data from Alphavantage: " + error);
+        return;
+      }
+      var jsonBody = JSON.parse(body);
+      if(jsonBody["Meta Data"]){        
+        addStockList(stock,jsonBody["Time Series (Daily)"]);
+        success();
+      } else {
+        failure();
+      } 
+    });
+    
+  }
+  //Checks to see whether stock is already included in globa stockList array
+  //Returns true if stock exists, false if not
+  function stockExists(stock) {
+    var bool = false;
+
+    for(var i = 0; i < stockList.length; i++ ){
+      if(stockList[i].index == stock) {
+        bool = true;
+      }
+    }
+    return bool;
+  }
+  //Send a message to client "Stock already added" with message code 41
+  function stockAlreadyAlert() {
+    var msg = {'message': 41, 'caption':"Stock already added"};
+    ws.send(JSON.stringify(msg));
+  }
+  //Send a message to client "Invalid stock Index" with message code 40
+  function invalidStockAlert() {
+    var msg = {'message': 40, 'caption':"Invalid stock index"};
+  
+    ws.send(JSON.stringify(msg));
+  }
+  //Pushes StockList to client of this connection
+  function pushStockList() {
+   var msg = {'message': 10,'stockList':stockList};
+  
+   ws.send(JSON.stringify(msg));
+  }
+  //Broadcast current stockList to All connected clients (except the client of this connection)
+  function pushStockListAll() {
+    var msg = {'message': 10,'stockList':stockList};
+    
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(msg));
+      }
+    });
+  }
+  //Removes the stockList element that corresponds to input stock
+  function removeStockList(stock) {
+    var index = -1;
+    for(var i = 0; i < stockList.length; i++ ) {
+      if(stock == stockList[i].index) {
+          index = i;
+      }
+    }
+    if(index >= 0 ) { 
+      stockList.splice(index,1);
+    }
+  }
+  //Takes stock index name and json data and pushes to global StockList array
+  function addStockList(newStock,data) {
+    var index = -1;
+    for(var i = 0; i < stockList.length; i++ ) {
+      if(newStock == stockList[i].index) {
+        index = i;
+      }
+    }
+    if(index < 0 ) {
+      stockList.push({"index":newStock,"data": data });
+    } 
+  }
+});
+
+
